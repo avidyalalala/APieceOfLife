8d6a8cdf76c7236749b5215dc7ee33f6a00a35b1
lina.hou <lina.hou@alibaba-inc.com>
2014-10-07 20:04:31 +0800

    分析它如何使用annotation来把一个类注册成 hsf 服务

--- biz/src/main/java/com/github/avidyalalala/hsf/HsfProviderScanner.java
+++ biz/src/main/java/com/github/avidyalalala/hsf/HsfProviderScanner.java
@@ -39,6 +39,9 @@ public class HsfProviderScanner extends ClassPathScanningCandidateComponentProvi
                 throw new RuntimeException(e);
             }
             String beanName = Introspector.decapitalize(AnnotationUtils.getAnnotation(type, HsfService.class).value().getSimpleName());
+            //AnnotationUtils 的使用，第一个参数必须是 Class，看type的赋值，明白此Class 是从bean上获得的，
+            // 则明白HSFProviderScanner 之所能扫到加了注释的类，是因为此类必须首先成为一个spring容器管理
+            // 的bean
             BeanDefinition providerDefinition = new GenericBeanDefinition();
             providerDefinition.setBeanClassName("com.taobao.hsf.app.spring.util.HSFSpringProviderBean");
             providerDefinition.getPropertyValues().add("serviceInterface", AnnotationUtils.getAnnotation(type, HsfService.class).value().getName());
