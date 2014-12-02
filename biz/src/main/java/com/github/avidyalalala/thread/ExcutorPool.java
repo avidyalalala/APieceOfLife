594ca0f622336ca94d430fe113fb047ae90f1b11
lina.hou <lina.hou@alibaba-inc.com>
2014-12-02 11:19:21 +0800

    test multi thread successfully

--- biz/src/main/java/com/github/avidyalalala/thread/ExcutorPool.java
+++ biz/src/main/java/com/github/avidyalalala/thread/ExcutorPool.java
@@ -1,5 +1,6 @@
 package com.github.avidyalalala.thread;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -12,14 +13,41 @@ import java.util.concurrent.atomic.AtomicInteger;
  * To change this template use File | Settings | File Templates.
  */
 public class ExcutorPool {
+    public static final int threadCount=5;
+    public static ConcurrentHashMap<Integer,int[]> splitPool=new ConcurrentHashMap<Integer, int[]>();
+    public static String[] task={"零","一","二","三","四","五","六","七","八","九","十"};
+
     /**
-     * 换一下ThreadPool 里的数字 fixedPoolSize，与i的数字threadCount，看看会发生什么。
-     * @param args
+     * long[] 是 [0, 100)
+     * @param total
+     * @return
      */
+    public static void initSplitPool(int total){
+        int unit= (int) Math.floor(total/threadCount);
+
+        for(int i=0;i<threadCount-1;i++){
+            splitPool.put(i,new int[]{i*unit,(i+1)*unit});
+        }
+        splitPool.put(threadCount-1,new int[]{(threadCount-1)*unit, total});
+    }
+
+    public static void handleUnit(int start, int end, int signal){
+        System.out.println("thread "+signal+"is handling:");
+        for(int index=start;index<end;index++){
+            System.out.println(task[index]);
+        }
+    }
+  /**
+   * 换一下ThreadPool 里的数字 fixedPoolSize，与i的数字threadCount，看看会发生什么。
+   * @param args
+   */
     public static void main(String[] args) {
+
+        initSplitPool(task.length);
+
+
         final AtomicInteger index=new AtomicInteger(0);
         final int fixedPoolSize=5;
-        final int threadCount=10;
 
         //初始化线程池，为线程安排工作，分配工作数量
         ExecutorService executors = Executors.newFixedThreadPool(fixedPoolSize);
@@ -29,14 +57,18 @@ public class ExcutorPool {
                 @Override
                 public void run() {
                     try {
-                        System.out.println("finalI"+finalI);
                         int id = index.getAndIncrement();
-                        System.out.println(id);
+                        System.out.println("thread "+finalI+" getting the atomicInteger is "+id);
+
+                        int[] range= splitPool.get(finalI);
+                        handleUnit(range[0], range[1],finalI);
+
                     } catch (Exception e) {
                         e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                     }
                 }
             });
         }
+        executors.shutdown();
     }
 }
