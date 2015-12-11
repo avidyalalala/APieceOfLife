4a308beae45288e12af26a0569116f1a8ea141bc
侯丽娜 <houlina02@meituan.com>
2015-12-11 15:23:38 +0800

    add the ParallelInvoke

---
+++ biz/src/main/java/com/github/avidyalalala/thread/pojo/InvokeResult.java
@@ -0,0 +1,17 @@
+package com.github.avidyalalala.thread.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by houlina02 on 15/12/11.
+ */
+@Getter
+@Setter
+public class InvokeResult {
+    String path;
+    String port;
+    String query;
+    String protocal;
+    String host;
+}
